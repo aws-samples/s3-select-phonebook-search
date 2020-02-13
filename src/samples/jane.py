106403479751a254e2fa471109340f13549f55c0
David Green <24728183+davidmichaelgreen@users.noreply.github.com>
2020-02-13 18:20:58 +0900

    adding python samples

---
+++ src/samples/jane.py
@@ -0,0 +1,25 @@
+import boto3
+
+s3 = boto3.client('s3')
+
+resp = s3.select_object_content(
+    Bucket='s3select-demo',
+    Key='sample_data.csv',
+    ExpressionType='SQL',
+    Expression="SELECT * FROM s3object s where s.\"Name\" = 'Jane'",
+    InputSerialization = {'CSV': {"FileHeaderInfo": "Use"}, 'CompressionType': 'NONE'},
+    OutputSerialization = {'CSV': {}},
+)
+
+for event in resp['Payload']:
+    if 'Records' in event:
+        records = event['Records']['Payload'].decode('utf-8')
+        print(records)
+    elif 'Stats' in event:
+        statsDetails = event['Stats']['Details']
+        print("Stats details bytesScanned: ")
+        print(statsDetails['BytesScanned'])
+        print("Stats details bytesProcessed: ")
+        print(statsDetails['BytesProcessed'])
+        print("Stats details BytesReturned: ")
+        print(statsDetails['BytesReturned'])
